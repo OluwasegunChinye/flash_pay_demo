90c5aa504d7dbcde247d65dc718629d5aae7a4d7
oluwasegunchinye <oluwasegunchinye@gmail.com>
2023-02-02 15:50:10 +0100

    ui tools set up

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
+import { useCallback } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    const [fontsLoaded] = useFonts({
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+        "clash": require('./assets/fonts/ClashGrotesk-Regular.otf')
+    });
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
+    return (
+        <View
+            className=" flex-1 justify-center items-center"
+            onLayout={onLayoutRootView}
+        >
+            <Text className="font-[clash] text-xl text-dark">
+              Interface 1234567890
+            </Text>
+            <Text className="text-lg text-primary300 ">
+                Interface 1234567890
+            </Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './App.{js,jsx,ts,tsx}',
+        './src/**/*.{js,jsx,ts,tsx}',
+        './screens/**/*.{js,ts,jsx,tsx}',
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        extend: {},
+        colors: {
+            primary100: '#EFF5FF',
+            primary200: '#79ACF7',
+            primary300: '#2303E8',
+            primary400: '#160093',
+            dark: '#0A0A0A',
+            gray: '#9CA3AF',
+            success: '#22C55E',
+        },
+    },
+    plugins: [],
+};
